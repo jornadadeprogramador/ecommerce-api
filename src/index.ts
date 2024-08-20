ce379d3f5e0f4a1eb58690ab518ed74c466207c7
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-20 14:56:11 -0300

    Listagem (GET) e inclusão (POST) de usuários.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js");
+});
+
+let usuarios = [{
+    nome: "Derci Santos",
+    idade: 33
+}, {
+    nome: "João da Silva",
+    idade: 44
+}];
+
+app.get("/users", (req: Request, res: Response) => {
+    res.send(usuarios);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    usuarios.push(user);
+    res.send({
+        message: "Usuário criado com sucesso!"
+    });
 });
 
 app.listen(3000, () => {
