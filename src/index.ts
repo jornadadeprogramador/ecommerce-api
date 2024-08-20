7b64d6caa8bc2529af272b2ede437dcfa3c41ad4
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-20 16:16:31 -0300

    Parâmetros de rota com método PUT

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,13 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js");
 });
 
+type User = {
+    id: number; 
+    nome: string;
+    email: string;
+};
 let id = 0;
-let usuarios: {id: number, nome: string, email: string}[] = [];
+let usuarios: User[] = [];
 
 app.get("/users", (req: Request, res: Response) => {
     res.send(usuarios);
@@ -27,6 +32,17 @@ app.post("/users", (req: Request, res: Response) => {
     usuarios.push(user);
     res.send({
         message: "Usuário criado com sucesso!"
+    });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = req.body;
+    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+    usuarios[indexOf].nome = user.nome;
+    usuarios[indexOf].email = user.email;
+    res.send({
+        message: "Usuário alterado com sucesso!"
     });
 });
 
