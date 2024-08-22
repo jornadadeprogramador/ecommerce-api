d005fb792790648bbd5310b9a172a3dd032ed06c
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-22 15:47:39 -0300

    Persistência de dados no Firestore

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,11 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { getFirestore } from "firebase-admin/firestore";
 
 type User = {
-    id: number; 
+    id: number;
     nome: string;
     email: string;
 };
-let id = 0;
 let usuarios: User[] = [];
 
 export class UsersController {
@@ -19,12 +19,11 @@ export class UsersController {
         res.send(user);
     }
 
-    static save(req: Request, res: Response) {
+    static async save(req: Request, res: Response) {
         let user = req.body;
-        user.id = ++id;
-        usuarios.push(user);
+        const userSalvo = await getFirestore().collection("users").add(user);
         res.send({
-            message: "Usuário criado com sucesso!"
+            message: `Usuário ${userSalvo.id} criado com sucesso!`
         });
     }
 
