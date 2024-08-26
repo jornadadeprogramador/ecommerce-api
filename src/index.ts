0813333dbc34471d03c1df3eca5359fc81cc5b87
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-26 15:35:19 -0300

    Criação do middleware errorHandler.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { initializeApp } from "firebase-admin/app";
 import { routes } from "./routes/index";
+import { errorHandler } from "./middlewares/error-handler.middleware";
 
 initializeApp();
 const app = express();
 
 routes(app);
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).send({
-        message: "Erro Interno do Servidor"
-    });
-});
+errorHandler(app);
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000");
