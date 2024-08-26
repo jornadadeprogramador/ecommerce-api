46c45ff34117fb27af146ff85a04e64ee94c4d7e
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-26 15:24:48 -0300

    Captura de erros com try catch

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -9,52 +9,82 @@ type User = {
 
 export class UsersController {
     static async getAll(req: Request, res: Response) {
-        const snapshot = await getFirestore().collection("users").get();
-        const users = snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
-                ...doc.data()
-            };
-        });
-        res.send(users);
+        try {
+            const snapshot = await getFirestore().collection("users").get();
+            const users = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                };
+            });
+            res.send(users);
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
     }
 
     static async getById(req: Request, res: Response) {
-        let userId = req.params.id;
-        const doc = await getFirestore().collection("users").doc(userId).get();
-        res.send({
-            id: doc.id,
-            ...doc.data()
-        });
+        try {
+            let userId = req.params.id;
+            const doc = await getFirestore().collection("users").doc(userId).get();
+            res.send({
+                id: doc.id,
+                ...doc.data()
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
     }
 
     static async save(req: Request, res: Response) {
-        let user = req.body;
-        const userSalvo = await getFirestore().collection("users").add(user);
-        res.status(201).send({
-            message: `Usuário ${userSalvo.id} criado com sucesso!`
-        });
+        try {
+            let user = req.body;
+            const userSalvo = await getFirestore().collection("users").add(user);
+            res.status(201).send({
+                message: `Usuário ${userSalvo.id} criado com sucesso!`
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
     }
 
     static update(req: Request, res: Response) {
-        let userId = req.params.id;
-        let user = req.body as User;
+        try {
+            let userId = req.params.id;
+            let user = req.body as User;
 
-        getFirestore().collection("users").doc(userId).set({
-            nome: user.nome,
-            email: user.email
-        });
-        
-        res.send({
-            message: "Usuário alterado com sucesso!"
-        });
+            getFirestore().collection("users").doc(userId).set({
+                nome: user.nome,
+                email: user.email
+            });
+
+            res.send({
+                message: "Usuário alterado com sucesso!"
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
     }
 
     static async delete(req: Request, res: Response) {
-        let userId = req.params.id;
-        
-        await getFirestore().collection("users").doc(userId).delete();
+        try {
+            let userId = req.params.id;
 
-        res.status(204).end();
+            await getFirestore().collection("users").doc(userId).delete();
+
+            res.status(204).end();
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
     }
 }
