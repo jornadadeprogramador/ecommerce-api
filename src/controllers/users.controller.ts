3734d5696a724f567fb4824bef39ed52ce9fde3b
Derci Santos <contato@jornadadeprogramador.com.br>
2024-08-27 10:32:11 -0300

    Validação de dados com Celebrate e Joi

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
-import { ValidationError } from "../errors/validation.error";
 import { NotFoundError } from "../errors/not-found.error";
 
 type User = {
@@ -36,10 +35,6 @@ export class UsersController {
 
     static async save(req: Request, res: Response, next: NextFunction) {
         let user = req.body;
-        if (!user.email || user.email?.length === 0) {
-            throw new ValidationError("E-mail obrigatório!");
-        }
-
         const userSalvo = await getFirestore().collection("users").add(user);
         res.status(201).send({
             message: `Usuário ${userSalvo.id} criado com sucesso!`
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { UsersController } from "../controllers/users.controller";
 import asyncHandler from "express-async-handler";
+import { celebrate, Joi, Segments } from "celebrate";
 
 export const userRoutes = express.Router();
 
 userRoutes.get("/users", asyncHandler(UsersController.getAll));
 userRoutes.get("/users/:id", asyncHandler(UsersController.getById));
-userRoutes.post("/users", asyncHandler(UsersController.save));
+userRoutes.post("/users", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        nome: Joi.string().required(),
+        email: Joi.string().email().required()
+    })
+}), asyncHandler(UsersController.save));
 userRoutes.put("/users/:id", asyncHandler(UsersController.update));
 userRoutes.delete("/users/:id", asyncHandler(UsersController.delete));
