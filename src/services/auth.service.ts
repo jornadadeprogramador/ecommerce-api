69b752a84afd0eef1e90967ab7029daa5f2306cf
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-06 17:12:03 -0300

    Obter a credencial do usuário - Login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,19 +1,26 @@
 import { EmailAlreadyExistsError } from "../errors/email-already-exists.error";
 import { User } from "../models/user.model";
-import { getAuth, UserRecord } from "firebase-admin/auth";
+import { FirebaseAuthError, getAuth, UserRecord } from "firebase-admin/auth";
+import { getAuth as getFirebaseAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 
 export class AuthService {
 
-    create(user: User): Promise<UserRecord> {
-        return getAuth().createUser({
-            email: user.email,
-            password: user.password,
-            displayName: user.nome,
-        }).catch(err => {
-            if (err.code === "auth/email-already-exists") {
+    async create(user: User): Promise<UserRecord> {
+        try {
+            return await getAuth().createUser({
+                email: user.email,
+                password: user.password,
+                displayName: user.nome,
+            });
+        } catch (err) {
+            if (err instanceof FirebaseAuthError && err.code === "auth/email-already-exists") {
                 throw new EmailAlreadyExistsError();
             }
             throw err;
-        });
+        }
+    }
+
+    login(email: string, password: string): Promise<UserCredential> {
+        return signInWithEmailAndPassword(getFirebaseAuth(), email, password);
     }
 }
