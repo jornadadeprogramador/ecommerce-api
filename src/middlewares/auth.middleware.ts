64c3e02da7ff306c98fc2a4c08f20a7fc2213e37
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-06 19:25:34 -0300

    Validando o token no Firebase

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
 import { UnauthorizedError } from "../errors/unauthorized.error";
+import { DecodedIdToken, getAuth } from "firebase-admin/auth";
 
 export const auth = (app: express.Express) => {
-    app.use((req: Request, res: Response, next: NextFunction) => {
+    app.use(async (req: Request, res: Response, next: NextFunction) => {
         const token = req.headers.authorization?.split("Bearer ")[1];
 
         if (token) {
-            console.log(token);
-            return next();
+            try {
+                const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true)
+                console.log(decodeIdToken);
+                return next();
+            } catch (error) {
+                next(new UnauthorizedError());
+            }
         }
 
         next(new UnauthorizedError());
