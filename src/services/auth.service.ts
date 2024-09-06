4614d27d44800c204609d839a67d0e687041a12b
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-06 16:45:38 -0300

    Tratamento de erro para usuário existente.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { EmailAlreadyExistsError } from "../errors/email-already-exists.error";
 import { User } from "../models/user.model";
 import { getAuth, UserRecord } from "firebase-admin/auth";
 
@@ -8,6 +9,11 @@ export class AuthService {
             email: user.email,
             password: user.password,
             displayName: user.nome,
+        }).catch(err => {
+            if (err.code === "auth/email-already-exists") {
+                throw new EmailAlreadyExistsError();
+            }
+            throw err;
         });
     }
 }
