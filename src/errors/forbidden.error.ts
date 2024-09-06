9ad6498d5e2173ec8f165955e6a5061ac52af7b4
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-06 19:58:08 -0300

    Adicionar usuário logado na Request

---
+++ src/errors/forbidden.error.ts
@@ -0,0 +1,9 @@
+import { ErrorBase } from "./base.error";
+
+export class ForbiddenError extends ErrorBase {
+
+    constructor(message = "Não autorizado") {
+        super(403, message);
+    }
+    
+}
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 import { UnauthorizedError } from "../errors/unauthorized.error";
 import { DecodedIdToken, getAuth } from "firebase-admin/auth";
+import { UserService } from "../services/user.service";
+import { ForbiddenError } from "../errors/forbidden.error";
 
 export const auth = (app: express.Express) => {
     app.use(async (req: Request, res: Response, next: NextFunction) => {
@@ -12,8 +14,14 @@ export const auth = (app: express.Express) => {
 
         if (token) {
             try {
-                const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true)
-                console.log(decodeIdToken);
+                const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true);
+                
+                const user = await new UserService().getById(decodeIdToken.uid);
+                if (!user) {
+                    return next(new ForbiddenError());
+                }
+                req.user = user;
+
                 return next();
             } catch (error) {
                 next(new UnauthorizedError());
