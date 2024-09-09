fcf2686127369735e57ab9c04bf8404c218154ec
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-09 17:16:10 -0300

    Remoção do NextFuction dos Controllers

--- src/controllers/companies.controller.ts
+++ src/controllers/companies.controller.ts
@@ -1,25 +1,25 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { Company } from "../models/company.model";
 import { CompanyService } from "../services/company.service";
 
 export class CompaniesController {
-    static async getAll(req: Request, res: Response, next: NextFunction) {
+    static async getAll(req: Request, res: Response) {
         res.send(await new CompanyService().getAll());
     }
 
-    static async getById(req: Request, res: Response, next: NextFunction) {
+    static async getById(req: Request, res: Response) {
         let companyId = req.params.id;
         res.send(await new CompanyService().getById(companyId));
     }
 
-    static async save(req: Request, res: Response, next: NextFunction) {
+    static async save(req: Request, res: Response) {
         await new CompanyService().save(req.body);
         res.status(201).send({
             message: `Empresa criada com sucesso!`
         });
     }
 
-    static async update(req: Request, res: Response, next: NextFunction) {
+    static async update(req: Request, res: Response) {
         let companyId = req.params.id;
         let company = req.body as Company;
         await new CompanyService().update(companyId, company);
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,25 +1,25 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import { UserService } from "../services/user.service";
 
 export class UsersController {
-    static async getAll(req: Request, res: Response, next: NextFunction) {
+    static async getAll(req: Request, res: Response) {
         res.send(await new UserService().getAll());
     }
 
-    static async getById(req: Request, res: Response, next: NextFunction) {
+    static async getById(req: Request, res: Response) {
         let userId = req.params.id;
         res.send(await new UserService().getById(userId));
     }
 
-    static async save(req: Request, res: Response, next: NextFunction) {
+    static async save(req: Request, res: Response) {
         await new UserService().save(req.body);
         res.status(201).send({
             message: `Usuário criado com sucesso!`
         });
     }
 
-    static async update(req: Request, res: Response, next: NextFunction) {
+    static async update(req: Request, res: Response) {
         let userId = req.params.id;
         let user = req.body as User;
         await new UserService().update(userId, user);
@@ -28,7 +28,7 @@ export class UsersController {
         });
     }
 
-    static async delete(req: Request, res: Response, next: NextFunction) {
+    static async delete(req: Request, res: Response) {
         let userId = req.params.id;
         await new UserService().delete(userId);
         res.status(204).end();
