d5346f2241bc0b82b51cafe02acf478f6808f5e1
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-12 17:36:22 -0300

    Instalação da dependência file-type

--- src/services/upload-file.service.ts
+++ src/services/upload-file.service.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { getStorage, getDownloadURL } from "firebase-admin/storage";
+import { fileTypeFromBuffer } from "file-type";
 
 export class UploadFileService {
 
@@ -8,6 +9,9 @@ export class UploadFileService {
     async upload(base64: string): Promise<string> {
         const fileBuffer = Buffer.from(base64, "base64");
 
+        const fileType = await fileTypeFromBuffer(fileBuffer);
+
+        const fileName = `image.${fileType?.ext}`;
         fs.writeFileSync(fileName, fileBuffer);
 
         const bucket = getStorage().bucket("e-commerce-d1288.appspot.com");
