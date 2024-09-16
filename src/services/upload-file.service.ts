080aa07fe767c24e6f62c99e77055fbfc4f15e04
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-16 15:49:47 -0300

    Validando arquivos sem extensão ao fazer upload para o Cloud Storage

--- src/services/upload-file.service.ts
+++ src/services/upload-file.service.ts
@@ -2,6 +2,7 @@ import fs from "node:fs";
 import { getStorage, getDownloadURL } from "firebase-admin/storage";
 import { fileTypeFromBuffer } from "file-type";
 import { randomUUID } from "node:crypto";
+import { ValidationError } from "../errors/validation.error.js";
 
 export class UploadFileService {
 
@@ -11,6 +12,10 @@ export class UploadFileService {
         const fileBuffer = Buffer.from(base64, "base64");
 
         const fileType = await fileTypeFromBuffer(fileBuffer);
+        if (!fileType) {
+            throw new ValidationError("A extensão do arquivo não é válida!");
+        }
+
 
         const fileName = `${randomUUID().toString()}.${fileType?.ext}`;
         fs.writeFileSync(fileName, fileBuffer);
