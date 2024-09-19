119d53a4936b2c40992beab2b6ff5205afed283b
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-19 11:45:44 -0300

    Validação de Query Params com Celebrate

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import asyncHandler from "express-async-handler";
 import { celebrate, Segments } from "celebrate";
 import { ProductsController } from "../controllers/products.controller.js";
-import { newProductSchema, updateProductSchema } from "../models/product.model.js";
+import { newProductSchema, searchQuerySchema, updateProductSchema } from "../models/product.model.js";
 
 export const productRoutes = Router();
 
 productRoutes.get("/products", asyncHandler(ProductsController.getAll));
-productRoutes.get("/products/search", asyncHandler(ProductsController.search));
+productRoutes.get("/products/search", celebrate({ [Segments.QUERY]: searchQuerySchema }), asyncHandler(ProductsController.search));
 productRoutes.get("/products/:id", asyncHandler(ProductsController.getById));
 productRoutes.post("/products", celebrate({ [Segments.BODY]: newProductSchema }), asyncHandler(ProductsController.save));
 productRoutes.put("/products/:id", celebrate({ [Segments.BODY]: updateProductSchema }), asyncHandler(ProductsController.update));
