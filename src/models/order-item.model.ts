8eeb7ce9672521fa2f2ccd375f9564233e62c1ff
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-24 11:44:41 -0300

    Validação de dados do array de itens do pedido.

---
+++ src/models/order-item.model.ts
@@ -0,0 +1,16 @@
+import { Joi } from "celebrate";
+import { Product } from "./product.model.js";
+
+export type OrderItem = {
+    produto: Product;
+    qtde: number;
+    observacao: string;
+}
+
+export const orderItemSchema = Joi.object().keys({
+    produto: Joi.object().keys({
+        id: Joi.string().trim().required()
+    }).required(),
+    qtde: Joi.number().integer().positive().required(),
+    observacao: Joi.string().trim().allow(null).default(null)
+});
---
+++ src/models/order.model.ts
@@ -0,0 +1,46 @@
+import { Joi } from "celebrate";
+import { Company } from "./company.model.js";
+import { Customer, customerSchema } from "./customer.model.js";
+import { OrderItem, orderItemSchema } from "./order-item.model.js";
+import { PaymentMethod } from "./payment-method.model.js";
+import { Address, orderAddressSchema } from "./address.model.js";
+
+export type Order = {
+    empresa: Company;
+    cliente: Customer;
+    endereco: Address;
+    cpfCnpjCupom: string;
+    data: Date;
+    isEntrega: boolean;
+    formaPagamento: PaymentMethod;
+    taxaEntrega: number;
+    items: OrderItem[];
+    status: OrderStatus;
+};
+
+export enum OrderStatus {
+    pendente = 'pendente',
+    aprovado = 'aprovado',
+    entrega = 'entrega',
+    concluido = 'concluido',
+    cancelado = 'cancelado'
+};
+
+export const newOrderSchema = Joi.object().keys({
+    empresa: Joi.object().keys({
+        id: Joi.string().trim().required()
+    }).required(),
+    cliente: customerSchema.required(),
+    endereco: orderAddressSchema.required(),
+    cpfCnpjCupom: Joi.alternatives().try(
+        Joi.string().length(11).required(),
+        Joi.string().length(14).required()
+    ).default(null),
+    isEntrega: Joi.boolean().required(),
+    formaPagamento: Joi.object().keys({
+        id: Joi.string().trim().required()
+    }).required(),
+    taxaEntrega: Joi.number().min(0).required(),
+    items: Joi.array().min(1).items(orderItemSchema).required(),
+    status: Joi.string().only().allow(OrderStatus.pendente).default(OrderStatus.pendente)
+});
