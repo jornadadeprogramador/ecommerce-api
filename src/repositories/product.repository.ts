6022bbdb41fe0b455bae84ff800fa3552be1a92a
Derci Santos <contato@jornadadeprogramador.com.br>
2024-09-26 11:28:17 -0300

    Implementado o productConverter e aplicado no Repository

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,43 +1,29 @@
-import { CollectionReference, getFirestore, QuerySnapshot } from "firebase-admin/firestore";
-import { Product } from "../models/product.model.js";
+import { CollectionReference, getFirestore } from "firebase-admin/firestore";
+import { Product, productConverter } from "../models/product.model.js";
 
 export class ProductRepository {
 
-    private collection: CollectionReference;
+    private collection: CollectionReference<Product>;
 
     constructor() {
-        this.collection = getFirestore().collection("products");
+        this.collection = getFirestore()
+            .collection("products")
+            .withConverter(productConverter);
     }
 
     async getAll(): Promise<Product[]> {
         const snapshot = await this.collection.get();
-        return this.snapshotToArray(snapshot);
+        return snapshot.docs.map(doc => doc.data());
     }
 
     async search(categoriaId: string): Promise<Product[]> {
         const snapshot = await this.collection.where("categoria.id", "==", categoriaId).get();
-        return this.snapshotToArray(snapshot);
-    }
-
-    private snapshotToArray(snapshot: QuerySnapshot): Product[] {
-        return snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
-                ...doc.data()
-            };
-        }) as Product[];
+        return snapshot.docs.map(doc => doc.data());
     }
 
     async getById(id: string): Promise<Product | null> {
         const doc = await this.collection.doc(id).get();
-        if (doc.exists) {
-            return {
-                id: doc.id,
-                ...doc.data()
-            } as Product;
-        } else {
-            return null;
-        }
+        return doc.data() ?? null;
     }
 
     async save(product: Product) {
@@ -45,15 +31,7 @@ export class ProductRepository {
     }
 
     async update(product: Product) {
-        let docRef = this.collection.doc(product.id!);
-        await docRef.set({
-            nome: product.nome,
-            descricao: product.descricao,
-            preco: product.preco,
-            imagem: product.imagem,
-            categoria: product.categoria,
-            ativa: product.ativa
-        });
+        await this.collection.doc(product.id).set(product);
     }
 
     async delete(id: string) {
@@ -61,7 +39,10 @@ export class ProductRepository {
     }
 
     async getCountByCategoria(categoriaId: string): Promise<number> {
-        const countSnapshot = await this.collection.where("categoria.id", "==", categoriaId).count().get();
+        const countSnapshot = await this.collection
+            .where("categoria.id", "==", categoriaId)
+            .count()
+            .get();
         return countSnapshot.data().count;
     }
 }
