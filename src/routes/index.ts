32ae689f511a5e02cf816f3f69b51020b826be22
Derci Santos <contato@jornadadeprogramador.com.br>
2024-10-01 16:00:51 -0300

    Implementado um middleware para liberar as rotas que podem ser acessadas de forma anônima

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,9 +6,11 @@ import { categoryRoutes } from "./categories.route.js";
 import { productRoutes } from "./products.route.js";
 import { paymentMethodsRoutes } from "./payment-methods.route.js";
 import { orderRoutes } from "./orders.route.js";
+import { allowAnonymousUser } from "../middlewares/allow-anonymous-user.middleware.js";
 
 export const routes = (app: express.Express) => {
     app.use(express.json({ limit: "5mb" }));
+    app.use(allowAnonymousUser);
     app.use(authRoutes);
     app.use(userRoutes);
     app.use(companyRoutes);
