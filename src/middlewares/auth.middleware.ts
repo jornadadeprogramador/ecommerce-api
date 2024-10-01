10aa30820c6b1c73465a6d3032ef729bf6176542
Derci Santos <contato@jornadadeprogramador.com.br>
2024-10-01 15:50:00 -0300

    Liberado o login anônimo no auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,6 +3,7 @@ import { UnauthorizedError } from "../errors/unauthorized.error.js";
 import { DecodedIdToken, getAuth } from "firebase-admin/auth";
 import { UserService } from "../services/user.service.js";
 import { ForbiddenError } from "../errors/forbidden.error.js";
+import { NotFoundError } from "../errors/not-found.error.js";
 
 export const auth = (app: express.Express) => {
     app.use(async (req: Request, res: Response, next: NextFunction) => {
@@ -16,15 +17,19 @@ export const auth = (app: express.Express) => {
             try {
                 const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true);
 
-                const user = await new UserService().getById(decodeIdToken.uid);
-                if (!user) {
-                    return next(new ForbiddenError());
+                if (decodeIdToken.firebase.sign_in_provider === "anonymous") {
+                    return next();
                 }
-                req.user = user;
+
+                req.user = await new UserService().getById(decodeIdToken.uid);
 
                 return next();
             } catch (error) {
-                next(new UnauthorizedError());
+                if (error instanceof NotFoundError) {
+                    return next(new ForbiddenError());
+                } else {
+                    return next(new UnauthorizedError());
+                }
             }
         }
 
