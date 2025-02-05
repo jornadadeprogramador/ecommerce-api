8e24f18aa4afe74301061d5c5e9f98167bd51903
Derci Santos <contato@jornadadeprogramador.com.br>
2025-02-05 15:14:03 -0300

    Middleware para redirecionar para a rota do swagger

--- functions/src/routes/swagger-docs.route.ts
+++ functions/src/routes/swagger-docs.route.ts
@@ -1,7 +1,13 @@
-import { Express } from "express";
+import { Express, NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./../docs/swagger-output.json" assert {type: "json"};
 
 export const swaggerDocs = (app: Express) => {
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        if (req.path === "/docs") {
+            return res.redirect("/api/docs/");
+        }
+        next();
+    });
     app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 }
